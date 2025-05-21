76bda85e7fb7a7e6cc5cc89215aa0d133b17c9c7
Shudapp <dragosandrei9321@gmail.com>
2025-05-21 17:46:35 +0300

    Added a very basic interface along with a base for the other contributors to push their changes to

---
+++ main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+
+main(){
+
+source register.sh
+source login.sh
+source report.sh
+
+
+FM="Welcome !\n 1.Register\n 2.Login\n 3.Report\n 4.Help\n"
+
+
+echo -e $FM
+
+read text
+text=${text,,}
+if [[ $text == "1" || $text == "register" || $text == "inregistreaza" ]]; then
+    echo "Te inregistrezi"
+    register
+elif [[ $text == "2" || $text == "login" || $text == "logheaza" ]]; then
+    echo "Te loghezi"
+elif [[ $text == "3" || $text == "Report" || $text == "Raport" ]]; then
+    echo "Genereaza raport"
+else
+   main
+fi
+
+
+}
+
+
+
+main
---
+++ register.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+
+register() {
+ echo "Vei introduce, in ordinea urmatoare, Numele din aplicatie, Numele pentru login, Parola"
+ echo "Introduce numele tau: "
+ read name
+ #verifica daca exista deja numele
+ echo "Introduce username-ul"
+ read user
+ echo "Introduce parola"
+ read parola
+ #crypting
+ #verifica daca exista deja contul
+
+
+}
